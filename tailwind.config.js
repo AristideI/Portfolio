716b84f7d056cd5f78d718eefe4fb952227f2a03
AristideI <a.isingizwe@alustudent.com>
2023-08-26 14:38:47 +0200

    Latest Update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        blue1: "#0B1127",
+        blue2: "#51B4E4",
+        purple1: "#444"
+      }
+    },
+  },
+  plugins: [],
+}
+
