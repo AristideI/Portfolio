b9beec29afee2c4039071a2ab596115eae6ece47
AristideI <a.isingizwe@alustudent.com>
2023-10-21 11:44:36 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import Loader from "./components/loader/Loader";
+// import { gsap } from "gsap";
+// import { useState, useEffect } from "react";
+// import Loader from "./components/loader/Loader";
 import Hero from "./components/Hero/Hero";
-import { gsap } from "gsap";
+import About from "./components/About/About";
 function App() {
-  let [loader, setLoader] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 4000);
-  }, []);
+  // let [loader, setLoader] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoader(false);
+  //   }, 4000);
+  // }, []);
 
-  return loader ? (
-    <Loader />
-  ) : (
-    <section>
-      <Hero />
-    </section>
-  );
-
-  // return (
+  // return loader ? (
+  //   <Loader />
+  // ) : (
   //   <section>
   //     <Hero />
   //   </section>
   // );
+
+  return (
+    <section>
+      <Hero />
+      <About />
+    </section>
+  );
 }
 
 export default App;
