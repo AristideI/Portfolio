8abd56b3f519feaa181aeb9030a1ba2a48f6bdc7
AristideI <a.isingizwe@alustudent.com>
2023-12-12 18:51:58 +0200

    latest feature

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,16 +1,35 @@
 import "./Project.css";
-// import { gsap } from "gsap";
-// import ScrollTrigger from "gsap/ScrollTrigger";
-// import { useEffect } from "react";
-// gsap.registerPlugin(ScrollTrigger);
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
 export default function Project() {
+  let pro = useRef(null);
+  useEffect(() => {
+    gsap.fromTo(
+      pro.current,
+      { scale: 1 },
+      {
+        scale: 3,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".tri",
+          start: "start 35%",
+          toggleActions: "play pause continue none",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
   return (
-    <article className="h-[500vh] w-full bg-blue-950">
+    <article className="h-[500vh] w-full bg-blue-950 overflow-hidden tri">
       <div className="w-full relative">
         <img
           className="inner w-[20%] object-cover absolute top-[10vh] left-[40%]"
           src="/inner.svg"
           alt=""
+          ref={pro}
         />
         <img
           className="inner2 w-[30%] object-cover absolute top-0 left-[35%]"
